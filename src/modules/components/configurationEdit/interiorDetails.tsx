1cff281cc03447e2c88e70f4a87ceb0e004333d2
marin <marinpaunovic@gmail.com>
2022-06-10 15:42:41 +0200

    a

--- src/modules/components/configurationEdit/interiorDetails.tsx
+++ src/modules/components/configurationEdit/interiorDetails.tsx
@@ -20,56 +20,49 @@ const InteriorDetails = () => {
           {currentConfigChoice ? (
             <div className="editDetails__choice__second">biraš {currentConfigChoice}</div>
           ) : (
-            <div>
-              <div className="editDetails__choice">
-                <div className="editDetails__choice__items">
-                  <div
-                    title="seats"
-                    className="editDetails__choice__wrapper"
-                    onClick={(e) => setCurrentConfigChoice(e.currentTarget.title)}
-                  >
-                    <img
-                      src={require("../../../images/" + carConfig.interior.seats + ".png")}
-                      style={{ blockSize: "60px", borderRadius: "100%" }}
-                    />
-                    <span>
-                      <p className="editDetails__choice__wrapper__title">seats</p>
-                      <p className="editDetails__choice__wrapper__sub">paint color</p>
-                    </span>
-                  </div>
-                  <div
-                    title="dash"
-                    className="editDetails__choice__wrapper"
-                    onClick={(e) => setCurrentConfigChoice(e.currentTarget.title)}
-                  >
-                    <img
-                      src={require("../../../images/" + carConfig.interior.dash + ".png")}
-                      style={{ blockSize: "60px", borderRadius: "100%" }}
-                    />
-                    <span>
-                      <p className="editDetails__choice__wrapper__title">dash</p>
-                      <p className="editDetails__choice__wrapper__sub">paint color</p>
-                    </span>
-                  </div>
+            <div className="editDetails__choice">
+              <div className="editDetails__choice__items">
+                <div
+                  title="seats"
+                  className="editDetails__choice__wrapper"
+                  onClick={(e) => setCurrentConfigChoice(e.currentTarget.title)}
+                >
+                  <img
+                    src={require("../../../images/" + carConfig.interior.seats + ".png")}
+                    style={{ blockSize: "60px", borderRadius: "100%" }}
+                  />
+                  <span>
+                    <p className="editDetails__choice__wrapper__title">seats</p>
+                    <p className="editDetails__choice__wrapper__sub">paint color</p>
+                  </span>
                 </div>
-                <div>
-                  <div className="editDetails__choice__popup__wrapper">
-                    <PopupInfo text={"text info unutar edita"} className="editDetails__choice__popup" />
-                    <span className="editDetails__choice__popup__price">120000€</span>
-                  </div>
-                  <button
-                    className="editDetails__choice__button"
-                    onClick={() => setCurrentConfigPage(currentConfigPage + 1)}
-                  >
-                    <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
-                      Interior{" "}
-                      <span className="material-symbols-outlined" style={{ color: "#FCFCFD", fontSize: "16px" }}>
-                        keyboard_arrow_right
-                      </span>
-                    </div>
-                  </button>
+                <div
+                  title="dash"
+                  className="editDetails__choice__wrapper"
+                  onClick={(e) => setCurrentConfigChoice(e.currentTarget.title)}
+                >
+                  <img
+                    src={require("../../../images/" + carConfig.interior.dash + ".png")}
+                    style={{ blockSize: "60px", borderRadius: "100%" }}
+                  />
+                  <span>
+                    <p className="editDetails__choice__wrapper__title">dash</p>
+                    <p className="editDetails__choice__wrapper__sub">paint color</p>
+                  </span>
                 </div>
               </div>
+              <div className="editDetails__choice__popup__wrapper">
+                <PopupInfo text={"text info unutar edita"} className="editDetails__choice__popup" />
+                <span className="editDetails__choice__popup__price">120000€</span>
+              </div>
+              <button className="editDetails__choice__button" onClick={() => setCurrentConfigPage(currentConfigPage + 1)}>
+                <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+                  Interior{" "}
+                  <span className="material-symbols-outlined" style={{ color: "#FCFCFD", fontSize: "16px" }}>
+                    keyboard_arrow_right
+                  </span>
+                </div>
+              </button>
             </div>
           )}
         </>
