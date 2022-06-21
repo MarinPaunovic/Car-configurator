7afe3866cb773f2a24e0df0f15c0b442296d80f3
marin <marinpaunovic@gmail.com>
2022-06-21 15:54:59 +0200

    a

--- src/pages/passwordReset.tsx
+++ src/pages/passwordReset.tsx
@@ -1,6 +1,6 @@
 import { sendPasswordResetEmail } from "firebase/auth";
 import { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { auth } from "../modules/auth/db";
 
 const PasswordReset = () => {
@@ -9,19 +9,43 @@ const PasswordReset = () => {
 
   return (
     <div>
-      <label>Password Reset</label>
-      <input type={"text"} placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)}></input>
-      <button
-        onClick={() => {
-          sendPasswordResetEmail(auth, email).then(() => {
-            alert("Password reset sent!");
-            setEmail("");
-            navigate("/login");
-          });
-        }}
-      >
-        Send
-      </button>
+      <div className="login">
+        <div className="login__wrapper">
+          <label className="login__label">Password Reset</label>
+          <div className="login__inputBox__wrapper">
+            <div className="passwordReset__test">
+              <div className="passwordReset__inputBox">
+                <img className="login__img" src={require("../images/loginImg.png")}></img>
+              </div>
+              Car configurator
+            </div>
+            <div className="passwordReset__input">
+              <input
+                className="passwordReset__input__email"
+                type={"text"}
+                placeholder="Email"
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+              ></input>
+              <button
+                className="login__buttons__loginButton"
+                onClick={() => {
+                  sendPasswordResetEmail(auth, email).then(() => {
+                    alert("Password reset sent!");
+                    setEmail("");
+                    navigate("/login");
+                  });
+                }}
+              >
+                Send
+              </button>
+              <p className="login__menu__register">
+                Head back to <Link to="/login">login</Link> page
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
