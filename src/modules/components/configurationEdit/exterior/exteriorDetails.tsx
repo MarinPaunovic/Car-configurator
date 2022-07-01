5b3f33d555d40f0f9bc6f830f8a5b93436a43509
marin <marinpaunovic@gmail.com>
2022-07-01 09:40:15 +0200

    changes in prettier configs,minor changes in code structure(useEffect),changed string concatenation to string interpolation on img src property

--- src/modules/components/configurationEdit/exterior/exteriorDetails.tsx
+++ src/modules/components/configurationEdit/exterior/exteriorDetails.tsx
@@ -1,110 +1,88 @@
-import { useEffect } from "react";
-import { useRecoilValue, useSetRecoilState } from "recoil";
-import { carCustomConfiguratorAtom, configuratorAtom, selectedCarAtom } from "../../../storage/carAtoms";
-import { getTitleAtom, localEditAtom, localEditSelector } from "../../../storage/editAtoms";
-import { optionsCurrentConfigAtom } from "../../../storage/optionsAtom";
-import { previewCurrentPageAtom } from "../../../storage/pageAtoms";
-import Pagination from "../../pagination/pagination";
-import EditChoiceComponent from "./editChoiceComponent";
-import EditChoiceSecondComponent from "./editChoiceSecondComponent";
+import { useEffect } from 'react'
+import { useRecoilValue, useSetRecoilState } from 'recoil'
+import { carCustomConfiguratorAtom, configuratorAtom, selectedCarAtom } from '../../../storage/carAtoms'
+import { getTitleAtom, localEditAtom, localEditSelector } from '../../../storage/editAtoms'
+import { optionsCurrentConfigAtom } from '../../../storage/optionsAtom'
+import { previewCurrentPageAtom } from '../../../storage/pageAtoms'
+import Pagination from '../../pagination/pagination'
+import EditChoiceComponent from './editChoiceComponent'
+import EditChoiceSecondComponent from './editChoiceSecondComponent/editChoiceSecondComponent'
 
 const ExteriorDetails = () => {
-  const carConfig = useRecoilValue(carCustomConfiguratorAtom);
-  const { carModel } = useRecoilValue(selectedCarAtom);
-  const currentPage = useRecoilValue(previewCurrentPageAtom);
-  const currentConfigPage = useRecoilValue(configuratorAtom);
-  const currentConfigChoice = useRecoilValue(optionsCurrentConfigAtom);
-  const setChoiceTitle = useSetRecoilState(getTitleAtom);
-  const localEdit = useRecoilValue(localEditAtom);
-  const editSelector = useRecoilValue(localEditSelector);
+	const carConfig = useRecoilValue(carCustomConfiguratorAtom)
+	const { carModel } = useRecoilValue(selectedCarAtom)
+	const currentPage = useRecoilValue(previewCurrentPageAtom)
+	const currentConfigPage = useRecoilValue(configuratorAtom)
+	const currentConfigChoice = useRecoilValue(optionsCurrentConfigAtom)
+	const setChoiceTitle = useSetRecoilState(getTitleAtom)
+	const localEdit = useRecoilValue(localEditAtom)
+	const editSelector = useRecoilValue(localEditSelector)
 
-  useEffect(() => {
-    if (currentConfigChoice) {
-      getTitle();
-    }
-  }, [currentConfigChoice]);
+	useEffect(() => {
+		const getTitle = () => {
+			switch (currentConfigChoice) {
+				case 'color':
+					setChoiceTitle('Paint color')
+					return
+				case 'wheels':
+					setChoiceTitle('Wheels')
+					return
+				case 'dash':
+					setChoiceTitle('Dash color')
+					return
+				case 'seats':
+					setChoiceTitle('Seats color')
+					return
+				default:
+					return
+			}
+		}
+		if (currentConfigChoice) {
+			getTitle()
+		}
+	}, [currentConfigChoice, setChoiceTitle])
 
-  const getTitle = () => {
-    switch (currentConfigChoice) {
-      case "color":
-        setChoiceTitle("Paint color");
-        return;
-      case "wheels":
-        setChoiceTitle("Wheels");
-        return;
-      case "dash":
-        setChoiceTitle("Dash color");
-        return;
-      case "seats":
-        setChoiceTitle("Seats color");
-        return;
-      default:
-        return;
-    }
-  };
+	return (
+		<>
+			{currentConfigPage && currentConfigPage === 1 && carConfig && (
+				<>
+					<div className="editDetails__wrapper">
+						{localEdit.edit && localEdit.edit.length !== 0 ? (
+							localEdit.edit === 'color' ? (
+								<>
+									{editSelector && (
+										<img
+											alt="car"
+											className="editDetails__img"
+											src={require(`../../../../images/${carModel}/exterior/${editSelector.value}/${carConfig.exterior.wheels}/${currentPage}.png`)}
+										/>
+									)}
+								</>
+							) : (
+								<>
+									{editSelector && (
+										<img
+											alt="car"
+											className="editDetails__img"
+											src={require(`../../../../images/${carModel}/exterior/${carConfig.exterior.color}/${editSelector.value}/${currentPage}.png`)}
+										/>
+									)}
+								</>
+							)
+						) : (
+							<img
+								alt="car"
+								className="editDetails__img"
+								src={require(`../../../../images/${carModel}/exterior/${carConfig.exterior.color}/${carConfig.exterior.wheels}/${currentPage}.png`)}
+							/>
+						)}
+						<Pagination pagesNumber={5} />
+					</div>
+					{currentConfigChoice ? <EditChoiceSecondComponent /> : <EditChoiceComponent />}
+				</>
+			)}
+		</>
+	)
+}
 
-  return (
-    <>
-      {currentConfigPage && currentConfigPage === 1 && carConfig && (
-        <>
-          <div className="editDetails__wrapper">
-            {localEdit.edit && localEdit.edit.length !== 0 ? (
-              localEdit.edit === "color" ? (
-                <>
-                  {editSelector && (
-                    <img
-                      className="editDetails__img"
-                      src={require("../../../../images/" +
-                        carModel +
-                        "/exterior/" +
-                        editSelector.value +
-                        "/" +
-                        carConfig.exterior.wheels +
-                        "/" +
-                        currentPage +
-                        ".png")}
-                    />
-                  )}
-                </>
-              ) : (
-                <>
-                  {editSelector && (
-                    <img
-                      className="editDetails__img"
-                      src={require("../../../../images/" +
-                        carModel +
-                        "/exterior/" +
-                        carConfig.exterior.color +
-                        "/" +
-                        editSelector.value +
-                        "/" +
-                        currentPage +
-                        ".png")}
-                    />
-                  )}
-                </>
-              )
-            ) : (
-              <img
-                className="editDetails__img"
-                src={require("../../../../images/" +
-                  carModel +
-                  "/exterior/" +
-                  carConfig.exterior.color +
-                  "/" +
-                  carConfig.exterior.wheels +
-                  "/" +
-                  currentPage +
-                  ".png")}
-              />
-            )}
-            <Pagination pagesNumber={5} />
-          </div>
-          {currentConfigChoice ? <EditChoiceSecondComponent /> : <EditChoiceComponent />}
-        </>
-      )}
-    </>
-  );
-};
-
-export default ExteriorDetails;
+export default ExteriorDetails
