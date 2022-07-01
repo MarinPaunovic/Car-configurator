e46c1efd75a0ae9af81d4c3292d73fe51caf9533
marin <marinpaunovic@gmail.com>
2022-07-01 10:08:06 +0200

    changes in prettier configs

--- src/modules/storage/deleteMessageAtom.ts
+++ src/modules/storage/deleteMessageAtom.ts
@@ -1,6 +1,6 @@
-import { atom } from "recoil";
+import { atom } from 'recoil'
 
 export const deleteMessageAtom = atom({
-  key: "deleteMessageAtom",
-  default: false,
-});
+	key: 'deleteMessageAtom',
+	default: false
+})
