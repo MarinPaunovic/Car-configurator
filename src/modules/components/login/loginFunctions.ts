0e087f4ec6f9d4d90f1273c6161a40eb9fabe016
marin <marinpaunovic@gmail.com>
2022-07-01 09:54:09 +0200

    changes in prettier configs

--- src/modules/components/login/loginFunctions.ts
+++ src/modules/components/login/loginFunctions.ts
@@ -1,49 +1,49 @@
 import {
-  browserLocalPersistence,
-  browserSessionPersistence,
-  setPersistence,
-  signInWithEmailAndPassword,
-} from "firebase/auth";
-import { addDoc, collection, getDocs, query, where } from "firebase/firestore";
-import { auth, db, singInWithGoogle } from "../../auth/db";
+	browserLocalPersistence,
+	browserSessionPersistence,
+	setPersistence,
+	signInWithEmailAndPassword
+} from 'firebase/auth'
+import { addDoc, collection, getDocs, query, where } from 'firebase/firestore'
+import { auth, db, singInWithGoogle } from '../../auth/db'
 
 const loginWithGoogle = async () => {
-  await singInWithGoogle().then(() => {
-    if (auth.currentUser) {
-      getDocs(query(collection(db, "Users"), where("uid", "==", auth.currentUser.uid))).then((data) => {
-        if (data.empty && auth.currentUser) {
-          addDoc(collection(db, "Users"), {
-            name: auth.currentUser.displayName,
-            email: auth.currentUser.email,
-            uid: auth.currentUser.uid,
-          });
-        }
-      });
-    }
-  });
-};
+	await singInWithGoogle().then(() => {
+		if (auth.currentUser) {
+			getDocs(query(collection(db, 'Users'), where('uid', '==', auth.currentUser.uid))).then((data) => {
+				if (data.empty && auth.currentUser) {
+					addDoc(collection(db, 'Users'), {
+						name: auth.currentUser.displayName,
+						email: auth.currentUser.email,
+						uid: auth.currentUser.uid
+					})
+				}
+			})
+		}
+	})
+}
 
 const loginWithEmailAndPassword = async (email: string, password: string) => {
-  await signInWithEmailAndPassword(auth, email, password).catch((e) => {
-    if (e.code === "auth/invalid-email") {
-      alert("Wrong email or password");
-    }
-  });
-};
+	await signInWithEmailAndPassword(auth, email, password).catch((e) => {
+		if (e.code === 'auth/invalid-email') {
+			alert('Wrong email or password')
+		}
+	})
+}
 
 export const loginHandler = async (remember: boolean, type: string, email?: string, password?: string) => {
-  if (type === "login") {
-    if (remember && email && password) {
-      await setPersistence(auth, browserLocalPersistence).then(async () => await loginWithEmailAndPassword(email, password));
-    } else if (email && password) {
-      await setPersistence(auth, browserSessionPersistence).then(
-        async () => await loginWithEmailAndPassword(email, password)
-      );
-    }
-  }
-  if (type === "google") {
-    if (remember) {
-      await setPersistence(auth, browserLocalPersistence).then(async () => await loginWithGoogle());
-    } else await setPersistence(auth, browserSessionPersistence).then(async () => await loginWithGoogle());
-  }
-};
+	if (type === 'login') {
+		if (remember && email && password) {
+			await setPersistence(auth, browserLocalPersistence).then(async () => await loginWithEmailAndPassword(email, password))
+		} else if (email && password) {
+			await setPersistence(auth, browserSessionPersistence).then(
+				async () => await loginWithEmailAndPassword(email, password)
+			)
+		}
+	}
+	if (type === 'google') {
+		if (remember) {
+			await setPersistence(auth, browserLocalPersistence).then(async () => await loginWithGoogle())
+		} else await setPersistence(auth, browserSessionPersistence).then(async () => await loginWithGoogle())
+	}
+}
