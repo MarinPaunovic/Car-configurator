569370283379f84e9e4a326fda4a4dcc78b21203
marin <marinpaunovic@gmail.com>
2022-07-01 09:36:21 +0200

    minor changes in code structure(useEffect),changes in prettier configs

--- src/modules/components/configurationEdit/editDetails.tsx
+++ src/modules/components/configurationEdit/editDetails.tsx
@@ -1,45 +1,40 @@
-import { useEffect, useRef } from "react";
-import { useRecoilValue, useSetRecoilState } from "recoil";
-import { carCustomConfiguratorAtom, configuratorAtom } from "../../storage/carAtoms";
-import { optionsCurrentConfigAtom } from "../../storage/optionsAtom";
-import { previewCurrentPageAtom } from "../../storage/pageAtoms";
+import { useEffect } from 'react'
+import { useRecoilValue, useSetRecoilState } from 'recoil'
+import { carCustomConfiguratorAtom, configuratorAtom } from '../../storage/carAtoms'
+import { optionsCurrentConfigAtom } from '../../storage/optionsAtom'
+import { previewCurrentPageAtom } from '../../storage/pageAtoms'
 
-import ExteriorDetails from "./exterior/exteriorDetails";
-import InteriorDetails from "./Interior/interiorDetails";
-import SummaryDetails from "./summary/summaryDetails";
+import ExteriorDetails from './exterior/exteriorDetails'
+import InteriorDetails from './Interior/interiorDetails'
+import SummaryDetails from './summary/summaryDetails'
 
 const EditDetails = () => {
-  const carConfig = useRecoilValue(carCustomConfiguratorAtom);
-  const setCurrentPage = useSetRecoilState(previewCurrentPageAtom);
-  const currentConfigPage = useRecoilValue(configuratorAtom);
-  const setCurrentConfigChoice = useSetRecoilState(optionsCurrentConfigAtom);
-  // let isMounted = useRef(false);
+	const carConfig = useRecoilValue(carCustomConfiguratorAtom)
+	const setCurrentPage = useSetRecoilState(previewCurrentPageAtom)
+	const currentConfigPage = useRecoilValue(configuratorAtom)
+	const setCurrentConfigChoice = useSetRecoilState(optionsCurrentConfigAtom)
 
-  useEffect(() => {
-    // if (!isMounted.current) {
-    //   isMounted.current = true;
-    //   return;
-    // }
-    return () => {
-      setCurrentPage(1);
-      setCurrentConfigChoice("");
-    };
-  }, []);
+	useEffect(() => {
+		return () => {
+			setCurrentPage(1)
+			setCurrentConfigChoice('')
+		}
+	}, [setCurrentPage, setCurrentConfigChoice])
 
-  return (
-    <div className="editDetails">
-      {currentConfigPage === 3 ? (
-        <SummaryDetails />
-      ) : (
-        carConfig && (
-          <div className="editDetails__new">
-            <InteriorDetails />
-            <ExteriorDetails />
-          </div>
-        )
-      )}
-    </div>
-  );
-};
+	return (
+		<div className="editDetails">
+			{currentConfigPage === 3 ? (
+				<SummaryDetails />
+			) : (
+				carConfig && (
+					<div className="editDetails__new">
+						<InteriorDetails />
+						<ExteriorDetails />
+					</div>
+				)
+			)}
+		</div>
+	)
+}
 
-export default EditDetails;
+export default EditDetails
