37a201af585280f4a86afc0f078e2f8f63f5f967
marin <marinpaunovic@gmail.com>
2022-07-01 10:20:16 +0200

    changes in prettier configs,minor changes in code structure(useEffect)

--- src/App.tsx
+++ src/App.tsx
@@ -1,56 +1,56 @@
-import { onAuthStateChanged } from "firebase/auth";
-import { useEffect } from "react";
-import { BrowserRouter as Router, Navigate, Route, Routes } from "react-router-dom";
-import { useRecoilState } from "recoil";
-import { auth } from "./modules/auth/db";
-import { userAtom } from "./modules/storage/userAtoms";
-import ConfigurationEdit from "./pages/configurationEdit";
-import ConfigurationView from "./pages/configurationView";
-import Homepage from "./pages/homepage";
-import Login from "./pages/login";
-import PasswordReset from "./pages/passwordReset";
-import Register from "./pages/register";
-import SelectCar from "./pages/selectCar";
+import { onAuthStateChanged } from 'firebase/auth'
+import { useEffect } from 'react'
+import { BrowserRouter as Router, Navigate, Route, Routes } from 'react-router-dom'
+import { useRecoilState } from 'recoil'
+import { auth } from './modules/auth/db'
+import { userAtom } from './modules/storage/userAtoms'
+import ConfigurationEdit from './pages/configurationEdit'
+import ConfigurationView from './pages/configurationView'
+import Homepage from './pages/homepage'
+import Login from './pages/login'
+import PasswordReset from './pages/passwordReset'
+import Register from './pages/register'
+import SelectCar from './pages/selectCar'
 
 const App = () => {
-  const [userInfo, setUserInfo] = useRecoilState(userAtom);
+	const [userInfo, setUserInfo] = useRecoilState(userAtom)
 
-  useEffect(() => {
-    const unsub = onAuthStateChanged(auth, (user) => {
-      if (user) {
-        setUserInfo({ name: user.displayName, uid: user.uid, email: user.email });
-        return;
-      }
-      setUserInfo({ name: "", email: "", uid: "" });
-    });
-    return () => {
-      unsub();
-    };
-  }, []);
+	useEffect(() => {
+		const unsub = onAuthStateChanged(auth, (user) => {
+			if (user) {
+				setUserInfo({ name: user.displayName, uid: user.uid, email: user.email })
+				return
+			}
+			setUserInfo({ name: '', email: '', uid: '' })
+		})
+		return () => {
+			unsub()
+		}
+	}, [setUserInfo])
 
-  return (
-    <Router>
-      <Routes>
-        {userInfo.email ? (
-          <>
-            <Route path="/" element={<Homepage />} />
-            <Route path="/configure" element={<SelectCar />} />
-            <Route path="/configuration-view" element={<ConfigurationView />} />
-            <Route path="/configuration-edit" element={<ConfigurationEdit />} />
-            <Route path="*" element={<Navigate to="/" />} />
-          </>
-        ) : (
-          <>
-            {" "}
-            <Route path="/login" element={<Login />} />
-            <Route path="/register" element={<Register />} />
-            <Route path="/password-reset" element={<PasswordReset />} />
-            <Route path="/*" element={<Navigate to="/login" />} />
-          </>
-        )}
-      </Routes>
-    </Router>
-  );
-};
+	return (
+		<Router>
+			<Routes>
+				{userInfo.email ? (
+					<>
+						<Route path="/" element={<Homepage />} />
+						<Route path="/configure" element={<SelectCar />} />
+						<Route path="/configuration-view" element={<ConfigurationView />} />
+						<Route path="/configuration-edit" element={<ConfigurationEdit />} />
+						<Route path="*" element={<Navigate to="/" />} />
+					</>
+				) : (
+					<>
+						{' '}
+						<Route path="/login" element={<Login />} />
+						<Route path="/register" element={<Register />} />
+						<Route path="/password-reset" element={<PasswordReset />} />
+						<Route path="/*" element={<Navigate to="/login" />} />
+					</>
+				)}
+			</Routes>
+		</Router>
+	)
+}
 
-export default App;
+export default App
