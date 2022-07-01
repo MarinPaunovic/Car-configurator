69466bdc78e59dd1297869596d9eeef0851f3880
marin <marinpaunovic@gmail.com>
2022-07-01 10:03:26 +0200

    changes in prettier configs,added alt property to img,minor changes in code( handleSubmit function,button) to fit the change

--- src/modules/components/register/registerInput.tsx
+++ src/modules/components/register/registerInput.tsx
@@ -1,72 +1,74 @@
-import { useState } from "react";
-import { Link } from "react-router-dom";
-import RememberMe from "../rememeberMe/rememberMe";
-import { onClick } from "./registerFunctions";
+import { useState } from 'react'
+import { Link } from 'react-router-dom'
+import RememberMe from '../rememeberMe/rememberMe'
+import { onClick } from './registerFunctions'
 
 const RegisterInput = () => {
-  const [name, setName] = useState("");
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  const [confirmPassword, setConfirmPassword] = useState("");
-  const [remember, setRemember] = useState(false);
+	const [name, setName] = useState('')
+	const [email, setEmail] = useState('')
+	const [password, setPassword] = useState('')
+	const [confirmPassword, setConfirmPassword] = useState('')
+	const [remember, setRemember] = useState(false)
 
-  return (
-    <div className="login">
-      <div className="login__wrapper">
-        <label className="register__label">Register Page</label>
-        <div className="login__inputBox__wrapper">
-          <div className="login__test">
-            <div>
-              <img className="login__img" src={require("../../../images/loginImg.png")}></img>
-            </div>
-            Car configurator
-          </div>
-          <div className="login__inputBox">
-            <input
-              className="register__name"
-              type={"text"}
-              placeholder={"Name"}
-              onChange={(e) => setName(e.target.value)}
-            ></input>
-            <input
-              className="register__email"
-              type={"text"}
-              placeholder={"E-mail"}
-              onChange={(e) => setEmail(e.target.value)}
-            ></input>
-            <input
-              className="register__password"
-              type={"password"}
-              placeholder={"Password"}
-              onChange={(e) => setPassword(e.target.value)}
-            ></input>
-            <input
-              className="register__confirmPassword"
-              type={"password"}
-              placeholder={"Confirm Password"}
-              onChange={(e) => setConfirmPassword(e.target.value)}
-            ></input>
-            <RememberMe remember={remember} setRemember={setRemember} />
-            <div className="login__buttons">
-              <button
-                className="login__buttons__loginButton"
-                onClick={async () => {
-                  await onClick(name, email, password, confirmPassword, remember);
-                }}
-              >
-                Register
-              </button>
-            </div>
-            <div className="login__menu">
-              <div className="login__menu__register">
-                Already have account? Login <Link to="/login">here</Link>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
-};
+	const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+		e.preventDefault()
+		await onClick(name, email, password, confirmPassword, remember)
+	}
 
-export default RegisterInput;
+	return (
+		<div className="login">
+			<div className="login__wrapper">
+				<label className="register__label">Register Page</label>
+				<div className="login__inputBox__wrapper">
+					<div className="login__test">
+						<div>
+							<img alt="login car" className="login__img" src={require('../../../images/loginImg.png')}></img>
+						</div>
+						Car configurator
+					</div>
+					<form onSubmit={(e) => handleSubmit(e)}>
+						<div className="login__inputBox">
+							<input
+								className="register__name"
+								type={'text'}
+								placeholder={'Name'}
+								onChange={(e) => setName(e.target.value)}
+							></input>
+							<input
+								className="register__email"
+								type={'text'}
+								placeholder={'E-mail'}
+								onChange={(e) => setEmail(e.target.value)}
+							></input>
+							<input
+								className="register__password"
+								type={'password'}
+								placeholder={'Password'}
+								onChange={(e) => setPassword(e.target.value)}
+							></input>
+							<input
+								className="register__confirmPassword"
+								type={'password'}
+								placeholder={'Confirm Password'}
+								onChange={(e) => setConfirmPassword(e.target.value)}
+							></input>
+							<RememberMe remember={remember} setRemember={setRemember} />
+							<div className="login__buttons">
+								<button className="login__buttons__loginButton" type="submit">
+									Register
+								</button>
+							</div>
+							<div className="login__menu">
+								<div className="login__menu__register">
+									Already have account? Login <Link to="/login">here</Link>
+								</div>
+							</div>
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export default RegisterInput
