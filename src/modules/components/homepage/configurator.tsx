5319db549c4bb8b3117401b799bb481306c692e6
marin <marinpaunovic@gmail.com>
2022-07-01 09:48:05 +0200

    changes in prettier configs,added alt property to img

--- src/modules/components/homepage/configurator.tsx
+++ src/modules/components/homepage/configurator.tsx
@@ -1,18 +1,19 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom'
 
 const Configurator = () => {
-  return (
-    <div className="homepage__configurator">
-      <img
-        className="homepage__configurator__carImage"
-        src={require("../../../images/front-left2.png")}
-        style={{ blockSize: "450px" }}
-      />
-      <div className="homepage__configurator__emptyMessage">
-        You haven't configured any cars yet. You may choose to <Link to="/configure">configure some now.</Link>
-      </div>
-    </div>
-  );
-};
+	return (
+		<div className="homepage__configurator">
+			<img
+				alt="car"
+				className="homepage__configurator__carImage"
+				src={require('../../../images/front-left2.png')}
+				style={{ blockSize: '450px' }}
+			/>
+			<div className="homepage__configurator__emptyMessage">
+				You haven't configured any cars yet. You may choose to <Link to="/configure">configure some now.</Link>
+			</div>
+		</div>
+	)
+}
 
-export default Configurator;
+export default Configurator
