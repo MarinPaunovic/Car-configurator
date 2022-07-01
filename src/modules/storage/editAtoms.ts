145d7080bc2ab894145d411d737bebe2843bcfca
marin <marinpaunovic@gmail.com>
2022-07-01 10:09:41 +0200

    changes in prettier configs, changed type into interface,savedCofigEditAtom default changed

--- src/modules/storage/editAtoms.ts
+++ src/modules/storage/editAtoms.ts
@@ -1,49 +1,49 @@
-import { atom, selector } from "recoil";
-import { persistAtom } from "./userAtoms";
+import { atom, selector } from 'recoil'
+import { persistAtom } from './userAtoms'
 
 export const getTitleAtom = atom({
-  key: "getTitle",
-  default: "",
-});
+	key: 'getTitle',
+	default: ''
+})
 
-type Edit = {
-  value: string;
-  edit: string;
-};
+interface Edit {
+	value: string
+	edit: string
+}
 
 export const localEditAtom = atom({
-  key: "localEditAtom",
-  default: <Edit>{},
-});
+	key: 'localEditAtom',
+	default: <Edit>{}
+})
 
 export const localEditSelector = selector({
-  key: "localEditSelector",
+	key: 'localEditSelector',
 
-  get: ({ get }) => {
-    const { value, edit } = get(localEditAtom);
-    switch (edit) {
-      case "color":
-        return { value, edit };
-      case "wheels":
-        return { value, edit };
-      case "seats":
-        return { value, edit };
-      case "dash":
-        return { value, edit };
-      default:
-        return;
-    }
-  },
-});
+	get: ({ get }) => {
+		const { value, edit } = get(localEditAtom)
+		switch (edit) {
+			case 'color':
+				return { value, edit }
+			case 'wheels':
+				return { value, edit }
+			case 'seats':
+				return { value, edit }
+			case 'dash':
+				return { value, edit }
+			default:
+				return
+		}
+	}
+})
 
 export const summaryAtom = atom({
-  key: "summaryAtom",
-  default: false,
-  effects_UNSTABLE: [persistAtom],
-});
+	key: 'summaryAtom',
+	default: false,
+	effects_UNSTABLE: [persistAtom]
+})
 
 export const savedConfigEditAtom = atom({
-  key: "savedConfigEditAtom",
-  default: <string>{},
-  effects_UNSTABLE: [persistAtom],
-});
+	key: 'savedConfigEditAtom',
+	default: '',
+	effects_UNSTABLE: [persistAtom]
+})
