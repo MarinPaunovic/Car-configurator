1874b5af27bbedcca79ab7c8ed45b00e2f33fffd
marin <marinpaunovic@gmail.com>
2022-07-01 10:03:52 +0200

    changes in prettier configs

--- src/modules/components/rememeberMe/rememberMe.tsx
+++ src/modules/components/rememeberMe/rememberMe.tsx
@@ -1,25 +1,25 @@
 interface IRememberMe {
-  remember: boolean;
-  setRemember: (remember: boolean) => void;
+	remember: boolean
+	setRemember: (remember: boolean) => void
 }
 
 const RememberMe = (props: IRememberMe) => {
-  const { remember, setRemember } = props;
-  return (
-    <div className="login__rememberMe">
-      Remember me
-      <div
-        className="login__rememberMe__checkbox"
-        onClick={() => setRemember(!remember)}
-        style={remember ? { background: "lightgreen" } : { backgroundColor: "lightgray" }}
-      >
-        <div
-          className="login__rememberMe_checkbox__slider"
-          style={remember ? { backgroundColor: "darkgreen", left: "21px" } : { backgroundColor: "#2e2e38", left: "0" }}
-        ></div>
-      </div>
-    </div>
-  );
-};
+	const { remember, setRemember } = props
+	return (
+		<div className="login__rememberMe">
+			Remember me
+			<div
+				className="login__rememberMe__checkbox"
+				onClick={() => setRemember(!remember)}
+				style={remember ? { background: 'lightgreen' } : { backgroundColor: 'lightgray' }}
+			>
+				<div
+					className="login__rememberMe_checkbox__slider"
+					style={remember ? { backgroundColor: 'darkgreen', left: '21px' } : { backgroundColor: '#2e2e38', left: '0' }}
+				></div>
+			</div>
+		</div>
+	)
+}
 
-export default RememberMe;
+export default RememberMe
