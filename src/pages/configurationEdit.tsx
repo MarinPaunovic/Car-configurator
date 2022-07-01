9d858023bd94a5c82fa105c589eea6ccb5edf19a
marin <marinpaunovic@gmail.com>
2022-07-01 10:11:08 +0200

    changes in prettier configs

--- src/pages/configurationEdit.tsx
+++ src/pages/configurationEdit.tsx
@@ -1,15 +1,15 @@
-import EditDetails from "../modules/components/configurationEdit/editDetails";
-import Options from "../modules/components/configurationView/options";
-import NavbarComponent from "../modules/components/navbar/navbarComponent";
+import EditDetails from '../modules/components/configurationEdit/editDetails'
+import Options from '../modules/components/configurationView/options'
+import NavbarComponent from '../modules/components/navbar/navbarComponent'
 
 const ConfigurationEdit = () => {
-  return (
-    <div className="configurationEdit">
-      <NavbarComponent />
-      <Options />
-      <EditDetails />
-    </div>
-  );
-};
+	return (
+		<div className="configurationEdit">
+			<NavbarComponent />
+			<Options />
+			<EditDetails />
+		</div>
+	)
+}
 
-export default ConfigurationEdit;
+export default ConfigurationEdit
