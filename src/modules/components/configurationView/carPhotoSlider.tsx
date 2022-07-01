f5c9d0e79493131e06471ff2d710963a5276ace5
marin <marinpaunovic@gmail.com>
2022-07-01 09:43:26 +0200

    changes in prettier configs,minor changes in code structure(useEffect),changed string concatenation to string interpolation on img src property

--- src/modules/components/configurationView/carPhotoSlider.tsx
+++ src/modules/components/configurationView/carPhotoSlider.tsx
@@ -1,66 +1,47 @@
-import { useEffect, useRef } from "react";
-import { useRecoilState, useRecoilValue } from "recoil";
-import { CarConfig, carCustomConfiguratorAtom, selectedCarAtom } from "../../storage/carAtoms";
-import { savedConfigEditAtom, summaryAtom } from "../../storage/editAtoms";
-import { previewCurrentPageAtom } from "../../storage/pageAtoms";
+import { useEffect } from 'react'
+import { useRecoilState, useRecoilValue } from 'recoil'
+import { CarConfig, carCustomConfiguratorAtom, selectedCarAtom } from '../../storage/carAtoms'
+import { savedConfigEditAtom, summaryAtom } from '../../storage/editAtoms'
+import { previewCurrentPageAtom } from '../../storage/pageAtoms'
 
-import Pagination from "../pagination/pagination";
+import Pagination from '../pagination/pagination'
 
 const CarPhotoSlider = () => {
-  const { carModel, color, wheels } = useRecoilValue(selectedCarAtom);
-  const savedConfigEdit = useRecoilValue(savedConfigEditAtom);
-  const [currentPage, setCurrentPage] = useRecoilState(previewCurrentPageAtom);
-  const summary = useRecoilValue(summaryAtom);
-  const carCustomConfig = useRecoilValue<CarConfig>(carCustomConfiguratorAtom);
-  let pages: number = 5;
-  // let isMounted = useRef(false);
-  useEffect(() => {
-    // if (!isMounted.current) {
-    //   isMounted.current = true;
-    //   return;
-    // }
-    return () => {
-      setCurrentPage(1);
-    };
-  }, []);
-  return (
-    <>
-      {color && (
-        <>
-          {summary || savedConfigEdit ? (
-            <img
-              className="configurationView__img"
-              src={require("../../../images/" +
-                carCustomConfig.carModel +
-                "/exterior/" +
-                carCustomConfig.exterior.color +
-                "/" +
-                carCustomConfig.exterior.wheels +
-                "/" +
-                currentPage +
-                ".png")}
-              style={{ blockSize: "300px" }}
-            />
-          ) : (
-            <img
-              className="configurationView__img"
-              src={require("../../../images/" +
-                carModel +
-                "/exterior/" +
-                color[0] +
-                "/" +
-                wheels[0] +
-                "/" +
-                currentPage +
-                ".png")}
-              style={{ blockSize: "300px" }}
-            />
-          )}
-          <Pagination pagesNumber={pages} />
-        </>
-      )}
-    </>
-  );
-};
+	const { carModel, color, wheels } = useRecoilValue(selectedCarAtom)
+	const savedConfigEdit = useRecoilValue(savedConfigEditAtom)
+	const [currentPage, setCurrentPage] = useRecoilState(previewCurrentPageAtom)
+	const summary = useRecoilValue(summaryAtom)
+	const carCustomConfig = useRecoilValue<CarConfig>(carCustomConfiguratorAtom)
+	let pages: number = 5
+	useEffect(() => {
+		return () => {
+			setCurrentPage(1)
+		}
+	}, [setCurrentPage])
+	return (
+		<>
+			{color && (
+				<>
+					{summary || savedConfigEdit ? (
+						<img
+							alt="car"
+							className="configurationView__img"
+							src={require(`../../../images/${carCustomConfig.carModel}/exterior/${carCustomConfig.exterior.color}/${carCustomConfig.exterior.wheels}/${currentPage}.png`)}
+							style={{ blockSize: '300px' }}
+						/>
+					) : (
+						<img
+							alt="car"
+							className="configurationView__img"
+							src={require(`../../../images/${carModel}/exterior/${color[0]}/${wheels[0]}/${currentPage}.png`)}
+							style={{ blockSize: '300px' }}
+						/>
+					)}
+					<Pagination pagesNumber={pages} />
+				</>
+			)}
+		</>
+	)
+}
 
-export default CarPhotoSlider;
+export default CarPhotoSlider
