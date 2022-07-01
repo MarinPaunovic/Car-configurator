ab1fc7448958190366b45b6ff6d7d744487dd2a5
marin <marinpaunovic@gmail.com>
2022-07-01 09:57:25 +0200

    changes in prettier configs,changed type into interface

--- src/modules/components/pagination/pagination.tsx
+++ src/modules/components/pagination/pagination.tsx
@@ -1,35 +1,35 @@
-import { useRecoilState } from "recoil";
-import { previewCurrentPageAtom } from "../../storage/pageAtoms";
+import { useRecoilState } from 'recoil'
+import { previewCurrentPageAtom } from '../../storage/pageAtoms'
 
-type Pages = {
-  pagesNumber: number;
-};
+interface Pages {
+	pagesNumber: number
+}
 
 const Pagination = (props: Pages) => {
-  const [currentPage, setCurrentPage] = useRecoilState(previewCurrentPageAtom);
-  const { pagesNumber } = props;
+	const [currentPage, setCurrentPage] = useRecoilState(previewCurrentPageAtom)
+	const { pagesNumber } = props
 
-  return (
-    <div className="pagination">
-      <button
-        className="pagination__button material-symbols-outlined"
-        onClick={() => setCurrentPage(currentPage - 1)}
-        disabled={currentPage === 1}
-      >
-        keyboard_arrow_left
-      </button>
-      <div className="pagination__currentPage">{currentPage}</div>
-      <div className="pagination__separator">/</div>
-      <div className="pagination__pagesNumber">{pagesNumber}</div>
-      <button
-        className="pagination__button material-symbols-outlined"
-        onClick={() => setCurrentPage(currentPage + 1)}
-        disabled={currentPage === pagesNumber}
-      >
-        keyboard_arrow_right
-      </button>
-    </div>
-  );
-};
+	return (
+		<div className="pagination">
+			<button
+				className="pagination__button material-symbols-outlined"
+				onClick={() => setCurrentPage(currentPage - 1)}
+				disabled={currentPage === 1}
+			>
+				keyboard_arrow_left
+			</button>
+			<div className="pagination__currentPage">{currentPage}</div>
+			<div className="pagination__separator">/</div>
+			<div className="pagination__pagesNumber">{pagesNumber}</div>
+			<button
+				className="pagination__button material-symbols-outlined"
+				onClick={() => setCurrentPage(currentPage + 1)}
+				disabled={currentPage === pagesNumber}
+			>
+				keyboard_arrow_right
+			</button>
+		</div>
+	)
+}
 
-export default Pagination;
+export default Pagination
