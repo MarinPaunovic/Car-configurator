ffe1028841a43ad28cf6942db16511d182af7a89
marin <marinpaunovic@gmail.com>
2022-07-01 09:54:39 +0200

    changes in prettier configs

--- src/modules/components/logout/logout.tsx
+++ src/modules/components/logout/logout.tsx
@@ -1,22 +1,22 @@
-import { signOut } from "firebase/auth";
-import { useSetRecoilState } from "recoil";
-import { auth } from "../../auth/db";
-import { savedConfigAtom } from "../../storage/carAtoms";
+import { signOut } from 'firebase/auth'
+import { useSetRecoilState } from 'recoil'
+import { auth } from '../../auth/db'
+import { savedConfigAtom } from '../../storage/carAtoms'
 
 const Logout = () => {
-  const savedConfig = useSetRecoilState(savedConfigAtom);
-  return (
-    <div>
-      <button
-        onClick={() => {
-          savedConfig({});
-          signOut(auth);
-        }}
-      >
-        Logout
-      </button>
-    </div>
-  );
-};
+	const savedConfig = useSetRecoilState(savedConfigAtom)
+	return (
+		<div>
+			<button
+				onClick={() => {
+					savedConfig({})
+					signOut(auth)
+				}}
+			>
+				Logout
+			</button>
+		</div>
+	)
+}
 
-export default Logout;
+export default Logout
