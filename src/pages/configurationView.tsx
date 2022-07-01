188fbfb52ca1a9591b8dbaf0d1db6af08ad5fe33
marin <marinpaunovic@gmail.com>
2022-07-01 10:11:46 +0200

    changes in prettier configs, minor changes in code structure(useEffect)

--- src/pages/configurationView.tsx
+++ src/pages/configurationView.tsx
@@ -1,30 +1,25 @@
-import Options from "../modules/components/configurationView/options";
-import NavbarComponent from "../modules/components/navbar/navbarComponent";
-import CarPhotoSlider from "../modules/components/configurationView/carPhotoSlider";
-import ConfigurationDetails from "../modules/components/configurationView/configurationDetails";
-import { useEffect, useRef } from "react";
-import { summaryAtom } from "../modules/storage/editAtoms";
-import { useSetRecoilState } from "recoil";
+import Options from '../modules/components/configurationView/options'
+import NavbarComponent from '../modules/components/navbar/navbarComponent'
+import CarPhotoSlider from '../modules/components/configurationView/carPhotoSlider'
+import ConfigurationDetails from '../modules/components/configurationView/configurationDetails'
+import { useEffect } from 'react'
+import { summaryAtom } from '../modules/storage/editAtoms'
+import { useSetRecoilState } from 'recoil'
 
 const ConfigurationView = () => {
-  const setSummary = useSetRecoilState(summaryAtom);
+	const setSummary = useSetRecoilState(summaryAtom)
 
-  // let isMounted = useRef(false);
-  useEffect(() => {
-    // if (!isMounted.current) {
-    //   isMounted.current = true;
-    //   return;
-    // }
-    setSummary(false);
-  }, []);
-  return (
-    <div className="configurationView">
-      <NavbarComponent />
-      <Options />
-      <CarPhotoSlider />
-      <ConfigurationDetails />
-    </div>
-  );
-};
+	useEffect(() => {
+		setSummary(false)
+	}, [setSummary])
+	return (
+		<div className="configurationView">
+			<NavbarComponent />
+			<Options />
+			<CarPhotoSlider />
+			<ConfigurationDetails />
+		</div>
+	)
+}
 
-export default ConfigurationView;
+export default ConfigurationView
