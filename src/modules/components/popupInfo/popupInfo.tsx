e86df5d3d589636b78e869872759afada2aece66
marin <marinpaunovic@gmail.com>
2022-07-01 09:58:54 +0200

    changes in prettier configs,changed type into interface

--- src/modules/components/popupInfo/popupInfo.tsx
+++ src/modules/components/popupInfo/popupInfo.tsx
@@ -1,28 +1,28 @@
-type Text = {
-  text: string;
-  className: string;
-};
+interface Text {
+	text: string
+	className: string
+}
 
 const PopupInfo = (props: Text) => {
-  const { text, className } = props;
-  return (
-    <div className={className}>
-      total
-      <div className="configurationDetails__header__price__wrapper">
-        <div
-          className={"configurationDetails__header__price__info__popup"}
-          style={className === "summary__popup" ? { marginTop: "-57px" } : { marginTop: "17px" }}
-        >
-          {text}
-        </div>
-        <span
-          className="material-symbols-outlined configurationDetails__header__price__info"
-          style={{ fontSize: "15px", fontVariationSettings: "1" }}
-        >
-          info
-        </span>
-      </div>
-    </div>
-  );
-};
-export default PopupInfo;
+	const { text, className } = props
+	return (
+		<div className={className}>
+			total
+			<div className="configurationDetails__header__price__wrapper">
+				<div
+					className={'configurationDetails__header__price__info__popup'}
+					style={className === 'summary__popup' ? { marginTop: '-57px' } : { marginTop: '17px' }}
+				>
+					{text}
+				</div>
+				<span
+					className="material-symbols-outlined configurationDetails__header__price__info"
+					style={{ fontSize: '15px', fontVariationSettings: '1' }}
+				>
+					info
+				</span>
+			</div>
+		</div>
+	)
+}
+export default PopupInfo
