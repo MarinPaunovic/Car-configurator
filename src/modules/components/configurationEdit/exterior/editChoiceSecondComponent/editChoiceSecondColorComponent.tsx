39443a9e3cc6d0511cd20c8d0722d541c26c3c91
marin <marinpaunovic@gmail.com>
2022-07-01 10:26:16 +0200

    changes in prettier configs, new file to separate parent component to smaller parts,added alt property to img,changed string concatenation to string interpolation on img src property

---
+++ src/modules/components/configurationEdit/exterior/editChoiceSecondComponent/editChoiceSecondColorComponent.tsx
@@ -0,0 +1,81 @@
+import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil'
+import { carCustomConfiguratorAtom, selectedCarAtom } from '../../../../storage/carAtoms'
+import { getTitleAtom, localEditAtom } from '../../../../storage/editAtoms'
+import { optionsCurrentConfigAtom } from '../../../../storage/optionsAtom'
+
+const EditChoiceSecondColorComponent = () => {
+	const setCurrentConfigChoice = useSetRecoilState(optionsCurrentConfigAtom)
+	const [localEdit, setLocalEdit] = useRecoilState(localEditAtom)
+	const carConfig = useRecoilValue(carCustomConfiguratorAtom)
+	const { color } = useRecoilValue(selectedCarAtom)
+	const title = useRecoilValue(getTitleAtom)
+	return (
+		<>
+			<div className="editDetails__choice__smallScreen__title">
+				{title}
+				<span
+					className="material-symbols-outlined editDetails__choice__smallScreen__title__x"
+					onClick={() => {
+						setCurrentConfigChoice('')
+						setLocalEdit({ value: '', edit: '' })
+					}}
+				>
+					x
+				</span>
+			</div>
+			{color.map((item: string, i: number) => (
+				<div
+					className="editDetails__choice__second__wrapper__each"
+					key={i}
+					onClick={() => {
+						setLocalEdit({ value: item, edit: 'color' })
+					}}
+				>
+					<div className="editDetails__choice__second__wrapper__color">
+						<div className="editDetails__choice__second__wrapper__color__mark">
+							<img
+								alt="car color"
+								src={require(`../../../../../images/colors/${item}.png`)}
+								style={{
+									blockSize: '60px',
+									borderRadius: '100%'
+								}}
+							/>
+							{localEdit.value
+								? item === localEdit.value && (
+										<div className="editDetails__choice__second__wrapper__chosen">
+											<span className="material-symbols-outlined editDetails__choice__second__wrapper__chosen__done">
+												done
+											</span>
+										</div>
+								  )
+								: carConfig &&
+								  item === carConfig.exterior.color && (
+										<div className="editDetails__choice__second__wrapper__chosen">
+											<span className="material-symbols-outlined editDetails__choice__second__wrapper__chosen__done">
+												done
+											</span>
+										</div>
+								  )}
+						</div>
+						<div className="editDetails__choice__second__wrapper__color__title_wrapper">
+							<span>{item}</span>
+							<>
+								{!localEdit.edit
+									? carConfig &&
+									  item === carConfig.exterior.color && (
+											<div className="editDetails__choice__second__wrapper__color__title__wrapper__price">2,500€</div>
+									  )
+									: item === localEdit.value && (
+											<div className="editDetails__choice__second__wrapper__color__title__wrapper__price">2,500€</div>
+									  )}
+							</>
+						</div>
+					</div>
+				</div>
+			))}
+		</>
+	)
+}
+
+export default EditChoiceSecondColorComponent
