c2992c2c62f6fd52742d16635149f5dd46075b49
marin <marinpaunovic@gmail.com>
2022-07-01 10:12:46 +0200

    changes in prettier configs, minor changes in code structure(useEffect,added onAuthStateChanged and removed auth.currentUser as dependency)

--- src/pages/homepage.tsx
+++ src/pages/homepage.tsx
@@ -1,81 +1,88 @@
-import { collection, onSnapshot, query, Unsubscribe, where } from "firebase/firestore";
-import { useEffect, useRef, useState } from "react";
-import { useRecoilState, useRecoilValue, useSetRecoilState } from "recoil";
-import { auth, db } from "../modules/auth/db";
-import Configurator from "../modules/components/homepage/configurator";
-import SavedConfigs from "../modules/components/homepage/savedConfigs";
-import NavbarComponent from "../modules/components/navbar/navbarComponent";
-import { ICar, savedConfigAtom, SavedConfigFetch } from "../modules/storage/carAtoms";
-import { deleteMessageAtom } from "../modules/storage/deleteMessageAtom";
-import { popupMenuAtom } from "../modules/storage/optionsAtom";
+import { onAuthStateChanged } from 'firebase/auth'
+import { collection, onSnapshot, query, Unsubscribe, where } from 'firebase/firestore'
+import { useEffect, useState } from 'react'
+import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil'
+import { auth, db } from '../modules/auth/db'
+import Configurator from '../modules/components/homepage/configurator'
+import SavedConfigs from '../modules/components/homepage/savedConfigs'
+import NavbarComponent from '../modules/components/navbar/navbarComponent'
+import { ICar, savedConfigAtom, SavedConfigFetch } from '../modules/storage/carAtoms'
+import { deleteMessageAtom } from '../modules/storage/deleteMessageAtom'
+import { popupMenuAtom } from '../modules/storage/optionsAtom'
 
 const Homepage = () => {
-  const [savedConfigs, setSavedConfig] = useRecoilState<ICar[]>(savedConfigAtom);
-  const deleteMessage = useRecoilValue(deleteMessageAtom);
-  const setPopupMenu = useSetRecoilState(popupMenuAtom);
-  const [loading, setLoading] = useState(true);
+	const [savedConfigs, setSavedConfig] = useRecoilState(savedConfigAtom)
+	const deleteMessage = useRecoilValue(deleteMessageAtom)
+	const setPopupMenu = useSetRecoilState(popupMenuAtom)
+	const [loading, setLoading] = useState(true)
+	useEffect(() => {
+		let unsub: Unsubscribe = () => {}
 
-  useEffect(() => {
-    let unsub: Unsubscribe = () => {};
-    if (auth.currentUser) {
-      unsub = onSnapshot(query(collection(db, "SavedConfigurations"), where("uid", "==", auth.currentUser.uid)), (snap) => {
-        const savedConfig = snap.docs.map((item) => {
-          let dataFetch: SavedConfigFetch = {
-            id: item.id,
-            carModel: item.data().carModel,
-            wheels: item.data().exterior.wheels,
-            seats: item.data().interior.seats,
-            dash: item.data().interior.dash,
-            color: item.data().exterior.color,
-            productionYear: item.data().productionYear,
-            createdAt: new Date(item.data().createdAt.seconds * 1000)
-              .toLocaleDateString("en", {
-                month: "short",
-                day: "2-digit",
-                year: "numeric",
-              })
-              .replace(",", "")
-              .split(" "),
-          };
-          return dataFetch;
-        });
-        setSavedConfig(savedConfig);
-        setLoading(false);
-      });
-    }
-    return () => {
-      setPopupMenu("");
-      unsub();
-    };
-  }, [auth.currentUser]);
+		onAuthStateChanged(auth, () => {
+			if (auth.currentUser) {
+				unsub = onSnapshot(
+					query(collection(db, 'SavedConfigurations'), where('uid', '==', auth.currentUser.uid)),
+					(snap) => {
+						const savedConfig = snap.docs.map((item) => {
+							let dataFetch: SavedConfigFetch = {
+								id: item.id,
+								carModel: item.data().carModel,
+								wheels: item.data().exterior.wheels,
+								seats: item.data().interior.seats,
+								dash: item.data().interior.dash,
+								color: item.data().exterior.color,
+								productionYear: item.data().productionYear,
+								createdAt: new Date(item.data().createdAt.seconds * 1000)
+									.toLocaleDateString('en', {
+										month: 'short',
+										day: '2-digit',
+										year: 'numeric'
+									})
+									.replace(',', '')
+									.split(' ')
+							}
+							return dataFetch
+						})
+						setSavedConfig(savedConfig)
+						setLoading(false)
+					}
+				)
+			}
+		})
 
-  return (
-    <div className="homepage">
-      <NavbarComponent />
+		return () => {
+			setPopupMenu('')
+			unsub()
+		}
+	}, [setPopupMenu, setSavedConfig])
 
-      {loading ? (
-        <div>Loading...</div>
-      ) : Object.keys(savedConfigs).length ? (
-        <div className="homepage__savedConfigs">
-          <div className="homepage__deleteMessage" style={deleteMessage ? { right: "0%" } : { right: "-200px" }}>
-            <p style={{ textAlign: "center" }}>Your configuration is successfully deleted!</p>
-          </div>
-          <div className="homepage__savedConfigs__title">View saved configurations</div>
-          <SavedConfigs />
-        </div>
-      ) : (
-        <>
-          <div
-            className="homepage__deleteMessage"
-            style={deleteMessage ? { right: "0%", top: "15%" } : { right: "-200px", top: "15%" }}
-          >
-            <p style={{ textAlign: "center" }}>Your configuration is successfully deleted!</p>
-          </div>
-          <Configurator />
-        </>
-      )}
-    </div>
-  );
-};
+	return (
+		<div className="homepage">
+			<NavbarComponent />
 
-export default Homepage;
+			{loading ? (
+				<div>Loading...</div>
+			) : Object.keys(savedConfigs).length ? (
+				<div className="homepage__savedConfigs">
+					<div className="homepage__deleteMessage" style={deleteMessage ? { right: '0%' } : { right: '-200px' }}>
+						<p style={{ textAlign: 'center' }}>Your configuration is successfully deleted!</p>
+					</div>
+					<div className="homepage__savedConfigs__title">View saved configurations</div>
+					<SavedConfigs />
+				</div>
+			) : (
+				<>
+					<div
+						className="homepage__deleteMessage"
+						style={deleteMessage ? { right: '0%', top: '15%' } : { right: '-200px', top: '15%' }}
+					>
+						<p style={{ textAlign: 'center' }}>Your configuration is successfully deleted!</p>
+					</div>
+					<Configurator />
+				</>
+			)}
+		</div>
+	)
+}
+
+export default Homepage
