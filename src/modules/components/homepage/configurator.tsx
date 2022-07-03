a412d011f0ab111bad2901d0e50fc0407d1e4e8d
marin <marinpaunovic@gmail.com>
2022-07-03 23:32:56 +0200

    removed style attribute on image

--- src/modules/components/homepage/configurator.tsx
+++ src/modules/components/homepage/configurator.tsx
@@ -3,12 +3,7 @@ import { Link } from 'react-router-dom'
 export const Configurator = () => {
 	return (
 		<div className="homepage__configurator">
-			<img
-				alt="car"
-				className="homepage__configurator__carImage"
-				src={require('images/front-left2.png')}
-				style={{ blockSize: '450px' }}
-			/>
+			<img alt="car" className="homepage__configurator__carImage" src={require('images/front-left2.png')} />
 			<div className="homepage__configurator__emptyMessage">
 				You haven't configured any cars yet. You may choose to <Link to="/configure">configure some now.</Link>
 			</div>
